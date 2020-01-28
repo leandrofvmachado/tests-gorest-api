71ce57ee1616e1c0bd6d0255370032b62af47bc0
Leandro Machado <leandromachado@tokenlab.com.br>
2020-01-27 23:47:49 -0300

    Added some user tests

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -7,6 +7,7 @@ import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -38,6 +39,38 @@ public class UserTest {
         assertUserIsDeleted(location, getResource(location, UserNotFoundResponseDTO.class));
     }
 
+    @Test
+    public void updateUser(){
+        UserRequestDTO user = createDummyUser();
+        UserRequestDTO userUpdated = createAnUpdatedDummyUser();
+        String location = createResource("/users", userUpdated);
+        UserResponseDTO retrievedUserUpdated = updateResource(location, UserResponseDTO.class);
+        assertEqualUser(userUpdated, retrievedUserUpdated);
+        deleteResource(location);
+    }
+
+    @Test
+    public void getMultipleUsers(){
+        int MAX_USERS = 10;
+        JsonPath genericGet = given()
+                .spec(spec)
+                .get("/users")
+                .then()
+                .statusCode(200)
+                .extract().jsonPath();
+        assertThat(genericGet.getList("result")).hasSizeGreaterThan(MAX_USERS);
+    }
+
+    @Test
+    public void getUserByName(){
+        UserRequestDTO newUser = createDummyUser();
+        String location = createResource("/users", newUser);
+        assertThat(getUserByName("Teste").getList("result")).hasSizeGreaterThanOrEqualTo(1);
+        deleteResource(location);
+    }
+
+    //TODO: negative tests
+
     private UserRequestDTO createDummyUser(){
         UserRequestDTO userRequestDTO = new UserRequestDTO();
         userRequestDTO.setFirst_name("Teste");
@@ -46,6 +79,17 @@ public class UserTest {
         userRequestDTO.setGender("female");
         userRequestDTO.setPhone("999999999");
         userRequestDTO.setStatus("active");
+        return userRequestDTO;
+    }
+
+    private UserRequestDTO createAnUpdatedDummyUser(){
+        UserRequestDTO userRequestDTO = new UserRequestDTO();
+        userRequestDTO.setFirst_name("Teste2");
+        userRequestDTO.setLast_name("Teste2");
+        userRequestDTO.setEmail("m@t.co");
+        userRequestDTO.setGender("male");
+        userRequestDTO.setPhone("111111111");
+        userRequestDTO.setStatus("inactive");
         return userRequestDTO;
     }
 
@@ -65,6 +109,25 @@ public class UserTest {
                 .spec(spec)
                 .when()
                 .get(location)
+                .then()
+                .statusCode(200)
+                .extract().as(responseClass);
+    }
+
+    private JsonPath getUserByName(String name){
+        return given()
+                .spec(spec)
+                .queryParam("first_name", name)
+                .when()
+                .get("/users")
+                .then()
+                .extract().jsonPath();
+    }
+    private <T> T updateResource(String location, Class<T> responseClass){
+        return given()
+                .spec(spec)
+                .when()
+                .put(location)
                 .then()
                 .statusCode(200)
                 .extract().as(responseClass);
